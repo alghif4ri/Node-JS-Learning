66d7da95375faa6203f0d8dc861505e46a6e9e11
LuckyNoobz <r.abudzar@gmail.com>
2023-12-25 11:42:38 +0700

    Contact Apps final

--- contactApp/app.js
+++ contactApp/app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
 require("./utils/db");
+
+const { body, validationResult, check } = require("express-validator");
+const methodOverride = require("method-override");
 const session = require("express-session");
 const cookieParser = require("cookie-parser");
 const flash = require("connect-flash");
@@ -8,6 +11,7 @@ const Contact = require("./model/contact");
 const app = express();
 const port = 3000;
 
+app.use(methodOverride("_method"));
 // setu EJS
 app.set("view engine", "ejs");
 app.use(expressLayouts);
@@ -66,9 +70,116 @@ app.get("/contact", async (req, res) => {
   });
 });
 
-app.get("/contact/:nama", async(req, res) => {
+app.get("/contact/add", (req, res) => {
+  res.render("add-contact", {
+    title: "Form add contact",
+    layout: "layouts/main-layouts",
+  });
+});
+
+app.post(
+  "/contact",
+  [
+    body("nama").custom(async (value) => {
+      const duplikat = await Contact.findOne({ nama: value });
+      if (duplikat) {
+        throw new Error("Nama sudah ada");
+      }
+      return true;
+    }),
+    check("email", "email tidak valid").isEmail(),
+    check("noHP", "No HP tidak valid").isMobilePhone("id-ID"),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.render("add-contact", {
+        title: "Form add contact",
+        layout: "layouts/main-layouts",
+        errors: errors.array(),
+      });
+    } else {
+      Contact.insertMany(req.body, (error, result) => {
+        req.flash("success_msg", "Data berhasil ditambahkan!");
+        res.redirect("/contact");
+      });
+    }
+  }
+);
+
+// app.get("/contact/delete/:nama", async (req, res) => {
+//   const contact = await Contact.findOne({ nama: req.params.nama });
+//   if (!contact) {
+//     res.status(404);
+//     res.send("<h1>404</h1>");
+//   } else {
+//     Contact.deleteOne({_id: contact._id}).then((result)=>{
+//       req.flash("success_msg", "Data berhasil dihapus!");
+//       res.redirect("/contact");
+//     });
+//   }
+// });
+
+app.delete("/contact", (req, res) => {
+  Contact.deleteOne({ nama: req.body.nama }).then((result) => {
+    req.flash("success_msg", "Data berhasil dihapus!");
+    res.redirect("/contact");
+  });
+});
+
+app.get("/contact/edit/:nama", async (req, res) => {
+  const contact = await Contact.findOne({ nama: req.params.nama });
+  res.render("edit-contact", {
+    title: "Form edit contact",
+    layout: "layouts/main-layouts",
+    contact,
+  });
+});
+
+app.put(
+  "/contact",
+  [
+    body("nama").custom(async (value, { req }) => {
+      const duplikat = await Contact.findOne({ nama: value });
+      if (value !== req.body.oldNama && duplikat) {
+        throw new Error("Nama sudah ada");
+      }
+      return true;
+    }),
+
+    check("email", "email tidak valid").isEmail(),
+    check("noHP", "No HP tidak valid").isMobilePhone("id-ID"),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.render("edit-contact", {
+        title: "Form edit contact",
+        layout: "layouts/main-layouts",
+        errors: errors.array(),
+        contact: req.body,
+      });
+    } else {
+      Contact.updateOne(
+        { _id: req.body._id },
+        {
+          $set: {
+            nama: req.body.nama,
+            email: req.body.email,
+            noHP: req.body.noHP,
+          },
+        }
+      ).then((result) => {
+        req.flash("success_msg", "Data berhasil diubah!");
+        res.redirect("/contact");
+      });
+    }
+  }
+);
+
+app.get("/contact/:nama", async (req, res) => {
   // const contact = findContact(req.params.nama);
-  const contact = await Contact.findOne({nama: req.params.nama})
+  const contact = await Contact.findOne({ nama: req.params.nama });
   // console.log(contacts);
   res.render("detail", {
     title: "Detail contact",
